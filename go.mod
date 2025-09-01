cd8be38d67ce98fd962d71f155ef77707461ed8d
Izaak Dale <izaakdaledev@gmail.com>
2025-09-01 00:11:45 -0700

    init commit of health server
    Signed-off-by: Izaak Dale <izaakdaledev@gmail.com>

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/izaakdale/hello-server
+
+go 1.24.2
+
+require (
+	github.com/izaakdale/ittp v0.1.0
+	github.com/rs/cors v1.11.1
+)
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/izaakdale/ittp"
+	"github.com/rs/cors"
+)
+
+func main() {
+	mux := ittp.NewServeMux()
+
+	mux.Get("/_/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("200 OK"))
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "80"
+	}
+
+	c := cors.New(cors.Options{
+		AllowedOrigins: []string{"*"},
+	})
+	mux.AddMiddleware(c.Handler)
+	log.Fatal(http.ListenAndServe(":"+port, mux))
+}
